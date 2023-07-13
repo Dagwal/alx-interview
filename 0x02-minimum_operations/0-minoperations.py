367a71c5b6e70761e8fd11d81f5dc29db51bfcf5
Dagwal <108694236+Dagwal@users.noreply.github.com>
2023-07-13 22:39:41 +0300

    Create 0-minoperations.py successfully

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    min_ops = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            min_ops += i
+            n = n / i
+        else:
+            i += 1
+    return min_ops
