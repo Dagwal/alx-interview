151e4a5634fdfda8d14382f5721e4fdbf0e1283a
Dagwal <108694236+Dagwal@users.noreply.github.com>
2023-07-13 22:46:32 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
-def minOperations(n):
+""" a method that calculates the fewest number of operations needed to
+result in exactly n H characters in the file. """
+
+
+def minOperations(n: int) -> int:
+    """
+    write a method that calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+
+    Argument:
+    n: integer
+    """
     if n <= 1:
         return 0
-
-    min_ops = 0
-    i = 2
-    while i <= n:
+    if n <= 5:
+        return n
+    for i in range(n//2, -1, -1):
         if n % i == 0:
-            min_ops += i
-            n = n / i
-        else:
-            i += 1
-    return min_ops
+            return minOperations(i) + (n // i)
+
+if __name__ == "__main__":
+    minOperations()
+
+    # iteration method
+    # count = 0
+    # for i in range(2, n + 1):
+    #     # check if problem can be broken into smaller problem
+    #     while not n % i:
+    #         # if yes then add no of smaller problems to result.
+    #         count += i
+    #         # create smaller problem
+    #         n /= i
+    # return count
+
+    # 1. We can find some largest number where n % k = 0.
+    # 2. Then find # of steps(copy+pastes) to reach k
+    # 3. Find number of pastes to reach n
+    # 4. answer = #2 + #3
