da434c4b7f52ed6a4f8b8c49b55fccf28b507f32
Dagwal <108694236+Dagwal@users.noreply.github.com>
2023-08-27 10:53:16 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,22 @@
+#!/usr/bin/python3
+
+""" determine the fewest number of coins needed to meet a given amount total"""
+
+
 def makeChange(coins, total):
+    """
+    Returns: fewest number of coins needed to meet total
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    
-    # Initialize a list to store the minimum number of coins for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    
-    for amount in range(1, total + 1):
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    
-    if dp[total] == float('inf'):
-        return -1
-    
-    return dp[total]
-
-# Test cases
-print(makeChange([1, 2, 25], 37))
-print(makeChange([1256, 54, 48, 16, 102], 1453))
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
